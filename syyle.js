aba805e0b3f9afc57505811564c21b071e1c8144
Willfred11 <110154225+Willfred11@users.noreply.github.com>
2024-02-06 13:10:15 +0100

    Add files via upload

---
+++ syyle.js
@@ -0,0 +1,6 @@
+const buttenE1 = document.querySelector('.btn1');
+const buttenE2 = document.querySelector('.btn2');
+
+
+buttenE1.addEventListener('click',open)
+buttenE2.addEventListener('click',open)
